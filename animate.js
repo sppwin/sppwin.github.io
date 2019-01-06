df2699e7c4e9a016cdfa464ef7f13d2abb78e4c3
madd512 <github@madd.tech>
2019-01-06 00:54:11 -0500

    l

--- animate.js
+++ animate.js
@@ -5,6 +5,7 @@ setInterval(function () {
   } else {
     frame += 1
   }
+  console.log(frame)
   document.querySelectorAll('.frame').style.display = 'none'
   document.querySelector('.frame' + frame).style.display = 'block'
 }, 500)
